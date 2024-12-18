25db4e282cd5b7f20ade5703053eb91a5febb2f5
StarkLin <liangyun.stark@gmail.com>
2024-12-18 16:31:54 +0800

    add custom pwa manifest

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,22 @@ export default defineConfig({
   plugins: [
     react(),
     legacy(),
-    VitePWA({ registerType: 'autoUpdate' })
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        id: 'poc-pwa-ionic-photo-gallery',
+        name: 'ionicPhotoGalleryPoc',
+        short_name: 'PhotoGallery',
+        description: 'poc pws ionic framework',
+        start_url: '/tab2',
+        display: 'standalone',
+        icons: [{
+          src: 'favicon.png',
+          type: 'image/png'
+        }],
+        lang: 'zh-Hant-TW',
+      }
+    })
   ],
   test: {
     globals: true,
